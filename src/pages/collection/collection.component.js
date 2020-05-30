a8e41bff861686429132b76d65e047eb31d222a1
zubair-ashraf <zubair8767844@gmail.com>
2020-05-30 19:53:25 +0500

    Initial

---
+++ src/pages/collection/collection.component.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { connect } from "react-redux";
+
+import CollectionItem from "../../components/collection-item/collection-item.component";
+
+import { selectCollection } from "../../redux/shop/shop.selectors";
+
+import "./collection.styles.scss";
+
+const CollectionPage = ({ collection, filteredItems }) => {
+  const { title } = collection;
+  let items = filteredItems.length <= 0 ? collection.items : filteredItems;
+
+  React.useEffect(() => {
+    filteredItems = [];
+    console.log(filteredItems, "filter");
+  }, []);
+
+  return (
+    <div className="collection-page">
+      <h2 className="title">{title}</h2>
+      <div className="items">
+        {items.map((item) => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state),
+  filteredItems: state.shop.filteredItems,
+});
+
+export default connect(mapStateToProps)(CollectionPage);
